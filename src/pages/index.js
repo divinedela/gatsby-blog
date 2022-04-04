f406525a0493740e8e9871a745123b4402fd08b1
Divine Dela <divinedela.dd@gmail.com>
2022-04-04 23:58:08 +0000

    display image with a gatsby plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout>
       <p>I'm making this by following the gatsby tutorial</p>
+      <StaticImage
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+      ></StaticImage>
     </Layout>
   );
 };
