c831a1b39ff34281f5d20920897c42bb705d6c26
Divine Dela <divinedela.dd@gmail.com>
2022-04-04 23:20:38 +0000

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main>
+      <title>About Page</title>
+      <h1>About Me</h1>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </main>
+  );
+};
+
+export default About;
