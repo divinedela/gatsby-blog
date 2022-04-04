0e0f7ba6ae4bfdac16b79e99d47f7c7104c6693c
Divine Dela <divinedela.dd@gmail.com>
2022-04-04 23:24:27 +0000

    add links between home and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const About = () => {
   return (
     <main>
       <title>About Page</title>
       <h1>About Me</h1>
+      <Link to="/">Link to Home Page</Link>
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p>
